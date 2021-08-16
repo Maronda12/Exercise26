34f613c5dfeb1d713349b5571404d07aa884a6bf
Maronda Anderson-Johnson <66698985+Maronda12@users.noreply.github.com>
2021-08-16 16:57:40 -0400

    Add files via upload

---
+++ Exercise26/Exercise26/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Exercise26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+            while (goOn == true)
+            {
+
+
+            Console.WriteLine("Please enter some text.");
+
+            string text = Console.ReadLine();
+
+            //Vowels uppercase/lowercase
+            string vowels = "aeiouAEIOU";
+            int total = 0;
+
+            //Checks for vowels and prints out
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (vowels.Contains(text[i] + ""))
+                {
+                    total++;
+                }
+            }
+
+            Console.WriteLine("Total vowels ");
+            Console.WriteLine("=============");
+            Console.WriteLine(total);
+
+
+                goOn = GetContinue(); // Calls Continue Method
+            }
+        }
+
+        public static bool GetContinue()
+        {
+            Console.WriteLine("Would you like to continue? y/n");
+            string answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else if (answer == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I didn't understand that, please try again");
+                
+                return GetContinue();
+            }
+        }
+    }
+}
